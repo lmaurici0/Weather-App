81e290b074290cab789f8e8fb99b20c74bb77668
Eric Luis <135241097+lmaurici0@users.noreply.github.com>
2023-11-02 20:04:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//Variables
+var input = document.querySelector("input");
+var button = document.querySelector("button");
+var place = document.querySelector("#place");
+var degrees = document.querySelector("#degrees");
+var img = document.querySelector("img");
+var wind = document.querySelector("#wind");
+var content = document.querySelector(".content");
+
+button.addEventListener("click", () => {
+  //Verify Input value
+  if (!input.value) return;
+  getDataApi();
+});
+
+async function getDataApi() {
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+    input.value
+  )}&units=metric&appid=7b6fb38e37268d898c9ab6432c23c2ec`;
+  await fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      loadData(data);
+    });
+}
+
+function loadData(data) {
+  place.innerText = `${data.name}, ${data.sys.country}`;
+  degrees.innerText = `Temperatura: ${Math.floor(data.main.temp)}° C`;
+  img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  wind.innerText = `Vento: ${data.wind.speed} km/h`;
+  content.style.display = "flex";
+}
